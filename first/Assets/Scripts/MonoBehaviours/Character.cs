12ca741472c110306609dee347ef68c2ca4fd2a1
rhayoon <hrhayoon@gmail.com>
2020-12-31 00:40:32 +0900

    first_2

---
+++ first/Assets/Scripts/MonoBehaviours/Character.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public int hitPoints;
+    public int maxHitPoints;
+
+    
+}
+public class Player : Character
+{ 
+    
+}
