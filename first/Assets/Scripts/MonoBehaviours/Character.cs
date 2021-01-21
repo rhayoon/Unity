74baf31d4fb4e512f3022e93e3b09a2ca04fbe92
rhayoon <hrhayoon@gmail.com>
2021-01-22 01:36:33 +0900

    uni

--- first/Assets/Scripts/MonoBehaviours/Character.cs
+++ first/Assets/Scripts/MonoBehaviours/Character.cs
@@ -4,19 +4,7 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-<<<<<<< HEAD
-    public int hitPoints;
-    public int maxHitPoints;
-
-    
-}
-public class Player : Character
-{ 
-    
-}
-=======
     public HitPoints hitPoints;
     public float maxHitPoints;
     public float startingHitPoints;
 }
->>>>>>> 464e454da641da69ae408f8828499c47c69893e7
